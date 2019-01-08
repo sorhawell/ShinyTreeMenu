c2846513511d124ff0c8b8a8a9acb9334aa46ad8
Søren Havelund Welling <sorhawell@gmail.com>
2019-01-08 14:16:14 +0100

    allow reactive level_icons similar to treedata
    should be backward compatible because disabled as default

--- R/updateShinyTreeMenu.R
+++ R/updateShinyTreeMenu.R
@@ -5,6 +5,7 @@
 #' @param treedata data.table; treedata - see details for more
 #' @param treemenu_open reactive expression; Id belonging to the branch you want to open.
 #' @param level_icons character; Names of fa-icons to use for the tree - see [ShinyTreeMenuHTML()] for more details.
+#' @param reactive_level_icons bool treat level_icons as unevaluated ractivem similar to treedata
 #'
 #' @return A HTML class character containing the tree menu
 #'
@@ -13,8 +14,12 @@
 #' @inherit renderShinyTreeMenu examples
 #'
 #' @export
-updateShinyTreeMenu <- function(treedata, treemenu_open, level_icons){
+updateShinyTreeMenu <- function(treedata, treemenu_open, level_icons,
+                                reactive_level_icons=FALSE){
+
+
   shiny::observeEvent(treemenu_open(), {
+    if(reactive_level_icons) level_icons = level_icons()
     shinyjs::html(id = paste0("treemenu_open_", treemenu_open()$openid),
                   html =
                     ShinyTreeMenu::ShinyTreeMenuHTML(
